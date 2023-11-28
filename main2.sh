ff89b3a3cd9ab12b8fe06b11263183ae5f69421c
shashank23088 <shashank23088@iiitd.ac.in>
2023-11-28 18:39:49 +0530

    added code for profiling for q2

---
+++ main2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function create_dir {
+  local dir_name=$1
+  
+  mkdir -p $dir_name
+  for i in {1..10000}; do
+    if ! dd if=/dev/zero of=${dir_name}/file${i}.txt bs=1 count=10; then
+      echo "Error creating file in ${dir_name}/file${i}.txt"
+      exit 1
+    else
+      echo "File created successfully: ${dir_name}/file${i}.txt"
+    fi
+  done
+}
+
+# Measure time taken for directory creation
+time_taken_dir1=$( { time create_dir dir1; } 2>&1 | grep real | awk '{print $2}' )
+time_taken_dir2=$( { time create_dir dir2; } 2>&1 | grep real | awk '{print $2}' )
+
+# Display results
+echo "Time taken for dir1: ${time_taken_dir1} seconds"
+echo "Time taken for dir2: ${time_taken_dir2} seconds"
