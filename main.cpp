4176ded33fdba88636514f3748d2beb5df836243
shashank23088 <shashank23088@iiitd.ac.in>
2023-11-29 19:37:39 +0530

    added multi-threading property for recursive options in external commands

--- main.cpp
+++ main.cpp
@@ -7,10 +7,17 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <fstream>
+#include <thread>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <mutex>
 
 using namespace std;
 
 class Shell {
+private:
+    mutex mtx; // Mutex for protecting shared resources during multi-threading
+
 public:
     void run() {
         while (true) {
@@ -56,7 +63,6 @@ public:
         }
     }
 
-private:
     bool containsHelpFlag(const vector<string>& args) {
         return find(args.begin(), args.end(), "-h") != args.end() ||
                find(args.begin(), args.end(), "--help") != args.end();
@@ -106,9 +112,10 @@ private:
             cout << "  -f                         force removal without confirmation\n";
         } else if (command == "cp") {
             cout << "cp - Copy files or directories.\n";
-            cout << "Usage: cp [-i] <source> <destination>\n";
+            cout << "Usage: cp [-i] [-r] <source> <destination>\n";
             cout << "Options:\n";
             cout << "  -i                         prompt before overwriting files\n";
+            cout << "  -r, --recursive            copy directories and their contents recursively\n";
         } else {
             cout << "No help available for command '" << command << "'.\n";
         }
@@ -172,7 +179,7 @@ private:
             // Additional information for the '-l' option
             if (longFormat) {
                 struct stat fileInfo;
-                if (stat(entry->d_name, &fileInfo) == 0) {
+                if (stat((path + "/" + entry->d_name).c_str(), &fileInfo) == 0) {
                     cout << " Size: " << fileInfo.st_size << " bytes";
                     // Add more information as needed
                 } else {
@@ -192,6 +199,56 @@ private:
         closedir(dir);
     }
 
+    void listDirectoryContentsRecursiveParallel(const string& path, bool showHidden, bool longFormat, bool recursive) {
+        DIR* dir = opendir(path.c_str());
+        if (dir == nullptr) {
+            perror("opendir");
+            return;
+        }
+
+        vector<thread> threads;
+        dirent* entry;
+        while ((entry = readdir(dir)) != nullptr) {
+            if (!showHidden && entry->d_name[0] == '.') {
+                continue; // Skip hidden files
+            }
+
+            threads.emplace_back(&Shell::processListDirectoryEntry, this, path, entry, showHidden, longFormat, recursive);
+        }
+
+        closedir(dir);
+
+        for (auto& thread : threads) {
+            thread.join();  
+        }
+    }
+
+    void processListDirectoryEntry(const string& path, dirent* entry, bool showHidden, bool longFormat, bool recursive) {
+        lock_guard<mutex> lock(mtx);
+
+        cout << entry->d_name;
+
+        // Additional information for the '-l' option
+        if (longFormat) {
+            struct stat fileInfo;
+            if (stat((path + "/" + entry->d_name).c_str(), &fileInfo) == 0) {
+                cout << " Size: " << fileInfo.st_size << " bytes";
+                // Add more information as needed
+            } else {
+                cerr << " Error getting file information for " << entry->d_name << endl;
+            }
+        }
+
+        cout << endl;
+
+        // Recursive listing for directories
+        if (recursive && isDirectory(path + "/" + entry->d_name)) {
+            cout << "  Subdirectory: " << entry->d_name << "\n";
+            listDirectoryContentsRecursiveParallel(path + "/" + entry->d_name, showHidden, longFormat, recursive);
+        }
+    }
+
+
     bool isDirectory(const string& path) {
         struct stat fileInfo;
         if (stat(path.c_str(), &fileInfo) != 0) {
@@ -208,6 +265,25 @@ private:
         return (stat(filename.c_str(), &buffer) == 0);
     }
 
+    void moveFileInternal(const string& source, const string& destination, bool recursive) {
+        if (fileExists(destination)) {
+            cerr << "Error: Destination file or directory already exists: " << destination << endl;
+            return;
+        }
+
+        if (rename(source.c_str(), destination.c_str()) != 0) {
+            perror("rename");
+            cerr << "Error: Unable to move file or directory to " << destination << endl;
+        } else {
+            cout << "Moved successfully\n";
+        }
+
+        if (recursive && isDirectory(source)) {
+            moveDirectoryContentsRecursive(source, destination);
+        }
+    }
+
+
     void moveFile(const vector<string>& args) {
         if (args.size() < 3 || args[1] == "-h" || args[1] == "--help") {
             showHelp("mv");
@@ -217,33 +293,71 @@ private:
         const string& source = args[1];
         const string& destination = args[2];
 
-        // Check if the source file exists
+        // Check if the source file or directory exists
         struct stat sourceInfo;
         if (stat(source.c_str(), &sourceInfo) != 0) {
             perror("stat");
-            cerr << "Error: Unable to access source file " << source << endl;
+            cerr << "Error: Unable to access source " << source << endl;
             return;
         }
 
         // Check if the destination is a directory
         struct stat destInfo;
         if (stat(destination.c_str(), &destInfo) == 0 && S_ISDIR(destInfo.st_mode)) {
-            // If the destination is a directory, append the source file's name to it
+            // If the destination is a directory, append the source file or directory's name to it
             const string destPath = destination + "/" + getFileName(source);
             if (rename(source.c_str(), destPath.c_str()) != 0) {
                 perror("rename");
-                cerr << "Error: Unable to move file to " << destPath << endl;
+                cerr << "Error: Unable to move " << source << " to " << destPath << endl;
             } else {
-                cout << "File moved successfully\n";
+                cout << "Moved successfully\n";
             }
         } else {
-            // If the destination is not a directory, move the file to the destination
+            // If the destination is not a directory, move the file or directory to the destination
             if (rename(source.c_str(), destination.c_str()) != 0) {
                 perror("rename");
-                cerr << "Error: Unable to move file to " << destination << endl;
+                cerr << "Error: Unable to move " << source << " to " << destination << endl;
             } else {
-                cout << "File moved successfully\n";
+                cout << "Moved successfully\n";
             }
+        }
+
+        // If the recursive option is specified, and the source is a directory, move its contents recursively
+        if (args.size() >= 4 && (args[3] == "-r" || args[3] == "--recursive") && isDirectory(source)) {
+            moveDirectoryContentsRecursive(source, destination);
+        }
+    }
+
+    void moveDirectoryContentsRecursive(const string& source, const string& destination) {
+        // Create the destination directory if it doesn't exist
+        if (mkdirRecursive(destination) != 0) {
+            cerr << "Error creating directory " << destination << endl;
+            return;
+        }
+
+        DIR* dir;
+        struct dirent* ent;
+
+        if ((dir = opendir(source.c_str())) != NULL) {
+            while ((ent = readdir(dir)) != NULL) {
+                if (string(ent->d_name) != "." && string(ent->d_name) != "..") {
+                    const string fullSourcePath = source + "/" + string(ent->d_name);
+                    const string fullDestPath = destination + "/" + string(ent->d_name);
+
+                    if (isDirectory(fullSourcePath)) {
+                        moveDirectoryContentsRecursive(fullSourcePath, fullDestPath);
+                    } else {
+                        // Move files (non-recursively) within the source directory to the destination
+                        moveFileInternal(fullSourcePath, fullDestPath, false);
+                    }
+                }
+            }
+            closedir(dir);
+
+            // Remove the source directory after its contents are moved
+            rmdir(source.c_str());
+        } else {
+            cerr << "Error opening directory " << source << endl;
         }
     }
 
@@ -333,76 +447,172 @@ private:
             return;
         }
 
-        if (args.size() < 3) {
-            cerr << "Usage: cp <source> <destination>\n";
-            return;
-        }
+        bool recursive = false;
+        bool interactive = false;
 
-        const string& source = args[1];
-        const string& destination = args[2];
-
-        // Check if the source file exists
-        ifstream sourceFile(source, ios::binary);
-        if (!sourceFile.is_open()) {
-            cerr << "Error: Unable to open source file " << source << endl;
-            return;
-        }
-
-        // Check if the destination is a directory
-        struct stat destInfo;
-        if (stat(destination.c_str(), &destInfo) == 0 && S_ISDIR(destInfo.st_mode)) {
-            // If the destination is a directory, append the source file's name to it
-            const string destPath = destination + "/" + getFileName(source);
-            copyFileContents(sourceFile, destPath, args);
-        } else {
-            // If the destination is not a directory, copy the file to the destination
-            copyFileContents(sourceFile, destination, args);
-        }
-
-        sourceFile.close();
-    }
-
-    void copyFileContents(ifstream& sourceFile, const string& destination, const vector<string>& args) {
-        // Check if the destination file exists
-        ofstream destFile(destination, ios::binary);
-        bool overwrite = false;
-
-        if (destFile.is_open()) {
-            destFile.close();
-
-            // Check if the '-i' option is provided for interactive prompting
-            for (size_t i = 3; i < args.size(); ++i) {
-                if (args[i] == "-i") {
-                    cout << "File " << destination << " already exists. Overwrite? (y/n): ";
-                    char response;
-                    cin >> response;
-                    overwrite = (response == 'y' || response == 'Y');
-                } else {
-                    cerr << "Unrecognized option: " << args[i] << endl;
-                    return;
-                }
+        // Skip flags when processing the command
+        size_t sourceIndex = 1;
+        size_t destIndex = 2;
+        if (args.size() > 3) {
+            if (args[1] == "-r" || args[1] == "--recursive") {
+                recursive = true;
+                sourceIndex = 2;
+                destIndex = 3;
+            } else if (args[1] == "-i") {
+                interactive = true;
+                sourceIndex = 2;
+                destIndex = 3;
             }
         }
 
-        if (overwrite || !destFile.is_open()) {
-            // Reset ifstream position before copying
-            sourceFile.clear();
-            sourceFile.seekg(0, ios::beg);
+        if (args.size() <= sourceIndex || args.size() <= destIndex) {
+            cerr << "cp: missing file operand\n";
+            showHelp("cp");
+            return;
+        }
 
-            // Copy file contents
-            destFile << sourceFile.rdbuf();
-            destFile.close();
+        const string& source = args[sourceIndex];
+        const string& destination = args[destIndex];
 
-            cout << "File copied successfully\n";
+        if (isDirectory(destination)) {
+            // Append the source directory name to the destination path
+            const string destPath = destination + "/" + getFileName(source);
+            recursiveCopyDirectoryParallel(source, destPath);
         } else {
-            cout << "File not overwritten.\n";
+            copyFileInternal(source, destination, recursive, interactive);
         }
     }
+
+    void copyFileInternal(const string& source, const string& destination, bool recursive, bool interactive) {
+        if (fileExists(destination)) {
+            if (interactive) {
+                cout << "cp: overwrite '" << destination << "'? (y/n): ";
+                char response;
+                cin >> response;
+
+                if (response != 'y') {
+                    cout << "cp: operation canceled for '" << destination << "'." << endl;
+                    return;
+                }
+            }
+
+            remove(destination.c_str());
+        }
+
+        ifstream sourceFile(source, ios::binary);
+        ofstream destFile(destination, ios::binary);
+
+        destFile << sourceFile.rdbuf();
+
+        sourceFile.close();
+        destFile.close();
+
+        cout << "File copied successfully\n";
+
+        if (recursive && isDirectory(source)) {
+            recursiveCopyDirectoryParallel(source, destination);
+        }
+    }
+
+    void recursiveCopyDirectory(const string& source, const string& destination) {
+        // Create the destination directory and intermediate directories if they don't exist
+        if (mkdirRecursive(destination) != 0) {
+            cerr << "Error creating directory " << destination << endl;
+            return;
+        }
+
+        DIR* dir;
+        struct dirent* ent;
+
+        if ((dir = opendir(source.c_str())) != NULL) {
+            while ((ent = readdir(dir)) != NULL) {
+                if (string(ent->d_name) != "." && string(ent->d_name) != "..") {
+                    const string fullSourcePath = source + "/" + string(ent->d_name);
+                    const string fullDestPath = destination + "/" + string(ent->d_name);
+
+                    if (isDirectory(fullSourcePath)) {
+                        recursiveCopyDirectory(fullSourcePath, destination); // Fix here: Use destination instead of fullDestPath
+                    } else {
+                        copyFileInternal(fullSourcePath, fullDestPath, true, false);
+                    }
+                }
+            }
+            closedir(dir);
+        } else {
+            cerr << "Error opening directory " << source << endl;
+        }
+    }
+
+
+    void recursiveCopyDirectoryParallel(const string& source, const string& destination) {
+        // Create the destination directory and intermediate directories if they don't exist
+        if (mkdirRecursive(destination) != 0) {
+            cerr << "Error creating directory " << destination << endl;
+            return;
+        }
+
+        DIR* dir;
+        struct dirent* ent;
+
+        if ((dir = opendir(source.c_str())) != NULL) {
+            vector<thread> threads;
+
+            while ((ent = readdir(dir)) != NULL) {
+                if (string(ent->d_name) != "." && string(ent->d_name) != "..") {
+                    const string fullSourcePath = source + "/" + string(ent->d_name);
+                    const string fullDestPath = destination + "/" + string(ent->d_name);
+
+                    if (isDirectory(fullSourcePath)) {
+                        threads.emplace_back(&Shell::processRecursiveCopyDirectoryEntry,
+                                            this, fullSourcePath, destination); // New function to handle subdirectories
+                    } else {
+                        copyFileInternal(fullSourcePath, fullDestPath, true, false);
+                    }
+                }
+            }
+
+            closedir(dir);
+
+            for (auto& thread : threads) {
+                thread.join();
+            }
+        } else {
+            cerr << "Error opening directory " << source << endl;
+        }
+    }
+
+    void processRecursiveCopyDirectoryEntry(const string& source, const string& destination) {
+        lock_guard<mutex> lock(mtx);
+
+        const string fullDestPath = destination + "/" + getFileName(source);
+        recursiveCopyDirectory(source, fullDestPath);
+    }
+
+
+    int mkdirRecursive(const string& path) {
+        size_t pos = 0;
+        while ((pos = path.find_first_of("/", pos)) != string::npos) {
+            string subpath = path.substr(0, pos);
+            if (mkdir(subpath.c_str(), 0777) != 0 && errno != EEXIST && !isDirectory(subpath)) {
+                perror("mkdir");
+                return -1;
+            }
+            pos++;
+        }
+
+        if (mkdir(path.c_str(), 0777) != 0 && errno != EEXIST && !isDirectory(path)) {
+            perror("mkdir");
+            return -1;
+        }
+        return 0;
+    }
+
 };
 
 int main() {
-    Shell myShell;
-    myShell.run();
+    Shell shell;
+    shell.run();
 
     return 0;
 }
+
