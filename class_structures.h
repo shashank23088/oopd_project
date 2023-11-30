7d6f8b5b8644dee5aeb0c89915e846ad1038ec0e
shashank23088 <shashank23088@iiitd.ac.in>
2023-11-30 19:50:13 +0530

    modularized main.cpp into multiple files

---
+++ class_structures.h
@@ -0,0 +1,24 @@
+#include "headers.h"
+
+class Shell {
+public:
+    void run();
+private:
+    bool containsHelpFlag(const vector<string>& args);
+    string getFileName(const string& path);
+    vector<string> parseCommand(const string& command);
+    void showHelp(const string& command);
+    void changeDirectory(const vector<string>& args);
+    void listDirectoryContents(const vector<string>& args) ;
+    void listDirectoryContentsRecursive(const string& path, bool showHidden, bool longFormat, bool recursive);
+    bool isDirectory(const string& path);
+    bool fileExists(const std::string& filename) ;
+    void moveFile(const vector<string>& args) ;
+    void removeFile(const vector<string>& args) ;
+    void removeDirectoryRecursive(const string& dir_path);
+    void copyFile(const vector<string>& args);
+    void copyFileContents(ifstream& sourceFile, const string& destination, const vector<string>& args) ;
+    void recursiveCopyDirectory(const string& source, const string& destination);
+    void copyFileContents(const string& source, const string& destination) ;
+    
+    };
---
+++ headers.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <cstring>
+#include <algorithm>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <fstream>
+
+using namespace std;
